911807ba22afde2db2f686c8524d947727fb4c6d
Muhammad Qasim Sher Khan Sherwani <mqasim111786111@gmail.com>
2026-02-21 23:29:57 +0500

    Refactor query engine setup and simplify ask_query

--- modules/query_engine.py
+++ modules/query_engine.py
@@ -1,18 +1,19 @@
-from llama_index.query_engine import SubQuestionQueryEngine, QueryEngineTool
-from llama_index.query_engine.tool_metadata import ToolMetadata
+# modules/query_engine.py
+from llama_index import QueryEngineTool, SubQuestionQueryEngine
 
 def get_query_engine(index):
     engine = index.as_query_engine()
-    query_tool = QueryEngineTool(
-        query_engine=engine,
-        metadata=ToolMetadata(
-            name="FinancialReport",
-            description="Provides insights from uploaded financial documents."
+    query_engine_tools = [
+        QueryEngineTool(
+            query_engine=engine,
+            metadata={
+                "name": "Annual Report",
+                "description": "Provides insights from company reports",
+            },
         )
-    )
-    s_engine = SubQuestionQueryEngine.from_defaults(query_engine_tools=[query_tool])
+    ]
+    s_engine = SubQuestionQueryEngine.from_defaults(query_engine_tools=query_engine_tools)
     return s_engine
 
-def ask_query(engine, query_text):
-    response = engine.query(query_text)
-    return response.response
+def ask_query(engine, query):
+    return engine.query(query).response
